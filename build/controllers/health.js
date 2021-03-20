96a35a31eeff5daf906b6ffaa45fcdad980128a2
Andre Burke <aburke.dev@gmail.com>
2021-03-19 22:51:17 -0600

    removed build folder

--- build/controllers/health.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var healthCheck = function (req, res, next) {
-    return res.status(200).json({
-        message: 'healthy'
-    });
-};
-exports.default = { healthCheck: healthCheck };
