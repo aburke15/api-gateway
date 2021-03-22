c1801bcb945ed2cc8e2fa39c61710c099d3c6948
Andre Burke <aburke.dev@gmail.com>
2021-03-21 23:41:45 -0600

    added validation with joi

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,26 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/User';
 import mongoose from 'mongoose';
-
-const authenticate = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({
-        message: 'authenticated'
-    });
-};
-
-const login = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({
-        message: 'logged in'
-    });
-};
-
-const token = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({
-        message: 'refresh token: blae23323423'
-    });
-};
+const Joi = require('@hapi/joi');
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
+    const schema = {
+        firstName: Joi.string().max(50),
+        lastName: Joi.string().max(50),
+        username: Joi.string().min(4).max(50).required(),
+        password: Joi.string().min(6).max(100).required(),
+        email: Joi.string().min(6).email().required(),
+        phone: Joi.string().min(10).max(11)
+    };
+
+    const { error } = Joi.validate(req.body, schema);
+    //console.log(error);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -33,8 +29,11 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const persistedUser = await user.save();
 
-        return res.status(200).json({
-            persistedUser
+        return res.status(200).send({
+            success: {
+                username: persistedUser.username,
+                email: persistedUser.email
+            }
         });
     } catch (err) {
         return res.status(400).json({
