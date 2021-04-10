c4e329d1382ecb692a61d7bf261dbe6cacabab3c
Andre Burke <aburkex24@gmail.com>
2021-04-10 00:56:08 -0600

    added enum type to help described logged errors

--- src/models/Log.ts
+++ src/models/Log.ts
@@ -1,5 +1,28 @@
 import mongoose from 'mongoose';
 
-const logSchema = new mongoose.Schema({});
+const logSchema = new mongoose.Schema({
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    level: {
+        type: String,
+        max: 10,
+        required: false
+    },
+    error: {
+        type: String
+    },
+    message: {
+        type: String
+    },
+    object: {
+        type: Object
+    },
+    type: {
+        type: String,
+        max: 50
+    }
+});
 
 export = mongoose.model('Log', logSchema);
